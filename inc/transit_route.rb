684c367902b25c5d434ec92c15b654ef42fa5569
Sebastien Guignot <kxu@tsys.org>
2016-05-04 17:31:54 +0200

    Improve shapes extrapolation.

--- inc/transit_route.rb
+++ inc/transit_route.rb
@@ -4,7 +4,6 @@ class TransitRoute
 		@id = id
 		@color = color
 		@edge_properties_map = {}
-		@segments = []
 	end
 
 	def add_edge(u, v)
@@ -14,47 +13,9 @@ class TransitRoute
 		end
 	end
 
-	def add_segment(segment)
-		@segments.push(segment) unless @segments.include?(segment)
-	end
-
-	def compute_unique_segments
-		sorted_edges = @edge_properties_map.map do |(u, v), property|
-			[u, v, property]
-		end.sort do |a, b|
-			a.last <=> b.last
-		end
-		graph = RGL::DirectedAdjacencyGraph.new
-		segments = []
-		sorted_edges.each do |(u, v, property)|
-			if !graph.has_vertex?(u) || !graph.has_vertex?(v) || graph.dijkstra_shortest_path(Hash.new(1), u, v).nil?
-				graph.add_edge(u, v)
-				segments.push(TransitSegment.new(u, v))
-			end
-		end
-
-		visitor = RGL::DFSVisitor.new(graph)
-		visitor.set_examine_edge_event_handler do |u, v|
-			add_segment(TransitSegment.new(u, v))
-		end
-		@segments = []
-		graph.depth_first_search(visitor) {}
-	end
-
-	def to_lines
-		lines = []
-		previous_node = nil
-		@segments.each do |segment|
-			lines.push([segment.a]) if previous_node != segment.a
-			lines.last.push(segment.b)
-			previous_node = segment.b
-		end
-		lines
-	end
-
 	def to_geojson_features
-		compute_unique_segments
-		to_lines.map do |line|
+		segments = compute_unique_segments
+		segments_to_lines(segments).map do |line|
 			{
 				'type' => 'Feature',
 				'properties' => {
@@ -67,6 +28,61 @@ class TransitRoute
 				}
 			}
 		end
+	end
+
+	private
+
+	def compute_unique_segments
+		sorted_edges = @edge_properties_map.map do |(u, v), property|
+			[u, v, property]
+		end.sort do |a, b|
+			a.last <=> b.last
+		end
+		graph = RGL::DirectedAdjacencyGraph.new
+		sorted_edges.each do |(u, v, property)|
+			new_segment = false
+			if !graph.has_vertex?(u) || !graph.has_vertex?(v) || (shortest_path = graph.dijkstra_shortest_path(Hash.new(1), u, v)).nil?
+				new_segment = true
+			else
+				shortest_path_properties = []
+				previous_node = nil
+				shortest_path.each do |node|
+					if !previous_node.nil?
+						key = [previous_node, node]
+						len = @edge_properties_map[key]
+						shortest_path_properties.push(len)
+					end
+					previous_node = node
+				end
+				shortest_path_len = shortest_path_properties.inject(&:+)
+				avg_edge_len = shortest_path_len.to_f / shortest_path_properties.size
+				new_segment = property < shortest_path_len / 5 || property < avg_edge_len
+			end
+			if new_segment
+				graph.add_edge(u, v)
+			end
+		end
+
+		segments = []
+		visitor = RGL::DFSVisitor.new(graph)
+		visitor.set_examine_edge_event_handler do |u, v|
+			segment = TransitSegment.new(u, v)
+			segments.push(segment) unless segments.include?(segment)
+		end
+		graph.depth_first_search(visitor) {}
+
+		return segments
+	end
+
+	def segments_to_lines(segments)
+		lines = []
+		previous_node = nil
+		segments.each do |segment|
+			lines.push([segment.a]) if previous_node != segment.a
+			lines.last.push(segment.b)
+			previous_node = segment.b
+		end
+		lines
 	end
 
 	def compute_distance(lon1, lat1, lon2, lat2)
